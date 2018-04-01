fdeb6aca45a5c3ecf7dfc1f72b279e3780e863c3
Christoph Haag <haagch@frickel.club>
2018-04-01 23:34:17 +0200

    add simple install script

---
+++ install_files_to_build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cp driver.vrdrivermanifest build/
+cp -r resources build/
+
+mkdir -p build/bin/{win64,win32,osx64,osx32,linux64,linux32}
+
+cp build/driver_openhmd.so build/bin/linux64 2> /dev/null && echo "Installed linux plugin!" || echo "Did not install linux plugin!"
+cp build/driver_openhmd.dll build/bin/win64 2> /dev/null && echo "Installed windows plugin!" || echo "Did not install windows plugin!"
