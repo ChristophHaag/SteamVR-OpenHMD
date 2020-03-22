ae978f64cf0bd414785a28cf2144b4e65e3ec285
Jan Schmidt <jan@centricular.com>
2020-03-23 07:58:00 +1100

    install the openhmd lib to the SteamVR build dir too.
    Make the ./install_files_to_build.sh script copy the libopenhmd
    subproject output into the deployment dir on Linux too, otherwise
    SteamVR ends up running against the system installed copy of
    OpenHMD

--- install_files_to_build.sh
+++ install_files_to_build.sh
@@ -5,5 +5,8 @@ cp -r resources build/
 
 mkdir -p build/bin/{win64,win32,osx64,osx32,linux64,linux32}
 
-cp build/driver_openhmd.so build/bin/linux64 2> /dev/null && echo "Installed linux plugin!" || echo "Did not install linux plugin!"
+cp build/driver_openhmd.so build/bin/linux64 2> /dev/null && \
+mkdir -p build/bin/linux64/subprojects/openhmd && cp build/subprojects/openhmd/libopenhmd.so.0 build/bin/linux64/subprojects/openhmd && \
+  echo "Installed linux plugin!" || echo "Did not install linux plugin!"
+
 cp build/driver_openhmd.dll build/bin/win64 2> /dev/null && echo "Installed windows plugin!" || echo "Did not install windows plugin!"
